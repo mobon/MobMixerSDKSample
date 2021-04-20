ddf5ee22081ec7450da367e8d0eef23dc7cb6ae1
mobon <53739373+mobon@users.noreply.github.com>
2021-04-20 12:41:51 +0900

    Delete mobmixer_lib directory

--- mobmixer_lib/src/main/java/com/mobmixer/sdk/BannerType.java
+++
@@ -1,9 +0,0 @@
-package com.mobmixer.sdk;
-
-public class BannerType {
-    public static final String BANNER_320x50 = "BANNER_320x50";
-    public static final String BANNER_320x100 = "BANNER_320x100";
-    public static final String BANNER_300x250 = "BANNER_300x250";
-
-
-}
